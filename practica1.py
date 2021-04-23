01e9c8083d2b9776b8c881f8fc148897096bbafa
adolfolm00 <adolfo.lopez.morales@alumnos.fesac.es>
2021-04-23 13:06:13 +0200

    hola

--- practica1.py
+++ practica1.py
@@ -1,1 +1,11 @@
 #Aquí quiero me de desarrolleis un programa que cuente desde un número introducido por pantalla hasta 1000, de tres en tres;
+num=(int(input("Escribe un número")))
+if(num>1000):
+ print("Has escrito un número mayor que 1000")
+
+while (num<=1000):
+ print (num)
+ num=num+3
+print("fin")
+
+
