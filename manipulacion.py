12ac06825c6e2bb60a54eec87c143652203c94c1
ramsal <ramon.salado@fesac.es>
2021-04-23 12:02:25 +0200

    Commit4

---
+++ manipulacion.py
@@ -0,0 +1,7 @@
+cadena = "En un Lugar de la Mancha"
+print (cadena[3])
+print (cadena[-1])
+print (cadena[3:-1])
+print (cadena[:4])
+print (cadena[3:])
+#Comentario
---
+++ mates.py
@@ -0,0 +1,3 @@
+b=2+8+10+11-20
+cadena = "El resultado es igual a:"
+print ( cadena , b)
