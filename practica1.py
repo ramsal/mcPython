37fd357dad5f4b23bb338d12d0c5035c9b14b4bb
JaimeME1 <71495467+JaimeME1@users.noreply.github.com>
2022-01-10 13:02:41 +0100

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -1,1 +1,7 @@
 #Aquí quiero me de desarrolleis un programa que cuente desde un número introducido por pantalla hasta 1000, de tres en tres;
+
+x = int(input('introduce un numero:'))
+
+while x<1000
+    x=x+3
+    print(x)
