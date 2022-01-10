52667fedd78df6aa27891ddc4b977e43015751d0
Fernando <fetmanditocamachito@gmail.com>
2022-01-10 12:26:16 +0100

    Ejercicios 1 y dos

--- practica1.py
+++ practica1.py
@@ -1,1 +1,16 @@
 #Aquí quiero me de desarrolleis un programa que cuente desde un número introducido por pantalla hasta 1000, de tres en tres;
+import sys
+
+def imprimir(start):
+    try:
+        for i in range(int(start),1000,3):
+            print(i)
+    except:
+        print("Introduce un valor numerico correcto")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv)<2:
+        print("Uso: python3 practica1 [longitud]")
+    else:
+        imprimir(sys.argv[1])
