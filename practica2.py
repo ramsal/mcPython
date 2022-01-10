df5164569d482e70d8989d04f76cb6ba0465d154
adolfolm00 <72855515+adolfolm00@users.noreply.github.com>
2022-01-10 13:16:40 +0100

    Update practica2.py

--- practica2.py
+++ practica2.py
@@ -1,1 +1,16 @@
 #Aquí quiero me de desarrolleis un programa con una función, que reciba una lista y un valor, y devulva las veces que aparece el valor en la lista;
+#! /usr/bin/python3
+numeros = [1,2,3,4,4,5,5,6]
+
+num = int(input("Escribe un valor:"))
+
+cont = 0
+
+for i in range(len(numeros)):
+    
+    if num == int(numeros[i]):
+        print(numeros[i])
+        cont = cont +1
+        
+
+print("El número:", num ,"aparece", cont ,"veces")
