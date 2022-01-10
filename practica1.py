6ad11f723e16343c6efe6c32df2ef0f22c772a3b
MrDreibus <36729553+MrDreibus@users.noreply.github.com>
2022-01-10 13:17:09 +0100

    practicas

--- practica1.py
+++ practica1.py
@@ -1,1 +1,8 @@
 #Aquí quiero me de desarrolleis un programa que cuente desde un número introducido por pantalla hasta 1000, de tres en tres;
+
+num = int(input("Diga un numero menor de mil"))
+
+while (num<=1000):
+	print (num)
+	num += 3
+
