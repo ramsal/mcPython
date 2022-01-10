a9313ebb2be4c24dade6bf963e330d13f5521fd2
mariamoraf <60214293+mariamoraf@users.noreply.github.com>
2022-01-10 14:22:33 +0100

    Update practica1.py

--- practica1.py
+++ practica1.py
@@ -1,1 +1,7 @@
 #Aquí quiero me de desarrolleis un programa que cuente desde un número introducido por pantalla hasta 1000, de tres en tres;
+a=int(input("Dime un numero > "))
+
+while (a<1000): 
+	print(a)
+	a= a+3
+print("Ya hemos terminado")
