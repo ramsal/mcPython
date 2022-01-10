61c206e1de22208cdf0744d8cc8190644bf8ae6d
JavierCasaisFedriani <71722331+JavierCasaisFedriani@users.noreply.github.com>
2022-01-10 12:52:54 +0100

    Create Practica2.py

---
+++ Practica2.py
@@ -0,0 +1,9 @@
+#Aquí quiero me de desarrolleis un programa con una función, que reciba una lista y un valor, y devulva las veces que aparece el valor en la lista;
+
+def funcionLista(lista, valor):
+	return lista.count(2)
+
+
+lista = [1,2,3,4,4,5,6,2,3,2,2]
+
+print (funcionLista(lista,2))
